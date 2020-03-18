f587af6019e4b3cab65db12c2b5d3599bfe63da4
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-03-18 08:14:24 +0100

    Initial Commit

---
+++ ListAppTests/ListAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ListAppTests.swift
+//  ListAppTests
+//
+//  Created by Mathis Detourbet on 18/3/20.
+//  Copyright © 2020 Mathis Detourbet. All rights reserved.
+//
+
+import XCTest
+@testable import ListApp
+
+class ListAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
